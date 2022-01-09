938e65a04687f9220a9096433a1bf2a34aac640d
mokou <mokou@fastmail.com>
2022-01-09 23:03:36 +0100

    feat: base json editor

---
+++ json-editor/Form.svelte
@@ -0,0 +1,118 @@
+<form>
+  <fieldset>
+    <label for="name">Name</label>
+    <input id="name" type="text" bind:value={name} />
+  </fieldset>
+  <fieldset>
+    <label for="level">Level</label>
+    <input id="level" type="number" max="60" bind:value={level} />
+  </fieldset>
+  <fieldset>
+    <label for="element">Element</label>
+    <select id="element" bind:value={element}>
+      <option value="fire">Fire</option>
+      <option value="water">Water</option>
+      <option value="earth">Earth</option>
+      <option value="lightning">Lightning</option>
+      <option value="ice">Ice</option>
+      <option value="wind">Wind</option>
+    </select>
+  </fieldset>
+  <fieldset>
+    <label for="area">Area</label>
+    <select id="area" bind:value={area}>
+      <option value="anemos">Anemos</option>
+      <option value="pagos">Pagos</option>
+      <option value="pyros">Pyros</option>
+      <option value="hydatos">Hydatos</option>
+    </select>
+  </fieldset>
+  <fieldset>
+    <input id="change" type="checkbox" checked={!!change} on:click={prepareChangeFields} />
+    <label for="change">Changes? (mutates/augments)</label>
+  </fieldset>
+  {#if change}
+    <fieldset>
+      <label for="changeType">Change type</label>
+      <select id="changeType" bind:value={change.type}>
+        <option value="mutates">Mutates</option>
+        <option value="augments">Augments</option>
+      </select>
+    </fieldset>
+    <fieldset>
+      <label for="changeDay">Change weathers DAY</label>
+      <select multiple id="changeDay" bind:value={change.day}>
+        <option value="0">Fair</option>
+        <option value="1">Showers</option>
+        <option value="2">Gales</option>
+        <option value="3">Blizzards</option>
+        <option value="4">Heat</option>
+        <option value="5">Thunder</option>
+        <option value="6">Gloom</option>
+        <option value="7">Snow</option>
+        <option value="8">Fog</option>
+        <option value="9">Umbral Wind</option>
+      </select>
+    </fieldset>
+    <fieldset>
+      <label for="changeNight">Change weathers NIGHT</label>
+      <select multiple id="changeNight" bind:value={change.night}>
+        <option value="0">Fair</option>
+        <option value="1">Showers</option>
+        <option value="2">Gales</option>
+        <option value="3">Blizzards</option>
+        <option value="4">Heat</option>
+        <option value="5">Thunder</option>
+        <option value="6">Gloom</option>
+        <option value="7">Snow</option>
+        <option value="8">Fog</option>
+        <option value="9">Umbral Wind</option>
+      </select>
+    </fieldset>
+  {/if}
+  <fieldset>
+    <label for="accuracy">How sure are we?</label>
+    <select id="accuracy" bind:value={accuracy}>
+      <option value="0">Not sure at all</option>
+      <option value="1">Reasonably sure</option>
+      <option value="2">Very sure</option>
+    </select>
+  </fieldset>
+  <button on:click={save}>Finish</button>
+</form>
+
+<script>
+import { nanoid } from 'nanoid';
+export let data;
+export let saveFn;
+export let isUpdate = false;
+
+let { level, name, id, element, change, area, accuracy } = data
+
+if (!id) {
+   id = nanoid()
+}
+
+function prepareChangeFields (evt) {
+   if (evt.target.checked) {
+     // prepare the necessary data structure for the checked key
+     change = { type: 'mutates', day: [], night: [] }
+   } else {
+     change = false
+   }
+}
+
+function save (evt) {
+   evt.preventDefault()
+
+  saveFn({
+    name,
+    level,
+    id,
+    element,
+    change,
+    area,
+    accuracy: Number(accuracy)
+  })
+}
+</script>
