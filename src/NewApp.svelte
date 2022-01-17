75681ae09087213b891353c95f336bcbdec6e82b
mokou <mokou@fastmail.com>
2022-01-17 12:53:46 +0100

    fix: re-add redirect banner

--- src/NewApp.svelte
+++ src/NewApp.svelte
@@ -12,11 +12,12 @@
   day.extend(relativeTime);
 
   const VERSION = "2.0.0";
+  const isRedirected = window.location.hash === "#redirect";
   $: ezTime = formatUtc($time);
 </script>
 
 <div class="bg-white">
-  <header class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 bg-gray-800 lg:drop-shadow-md">
+  <header class="mx-auto px-2 sm:px-6 lg:px-8 bg-gray-800 lg:drop-shadow-md">
     <div class="relative flex items-center justify-between h-16">
       <div class="flex-1 flex items-center justify-start">
         <div class="flex-shrink-0 flex items-center">
@@ -38,6 +39,11 @@
       </div>
     </div>
   </header>
+  {#if isRedirected}
+    <div class="text-center bg-red-500 text-white py-2">
+      <strong>Notice:</strong> The website URL has changed to eureka.fernehalwes.org! Please update your bookmarks.
+    </div>
+  {/if}
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-4">
     <Route path="/">
       <Tracker/>
