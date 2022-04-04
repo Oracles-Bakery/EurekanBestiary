c685a4a59025cd041decd9edb5bba882278db5cf
mokou <mokou@fastmail.com>
2022-04-04 16:19:15 +0200

    you have to track files if you want to commit them

---
+++ src/Forecast.svelte
@@ -0,0 +1,10 @@
+<script>
+  import ZoneForecast from "./components/ZoneForecast.svelte";
+</script>
+
+<h1 class="font-size-24 font-weight-bold">Forecast for Eureka</h1>
+
+<ZoneForecast zone="anemos" />
+<ZoneForecast zone="pagos" />
+<ZoneForecast zone="pyros" />
+<ZoneForecast zone="hydatos" />
