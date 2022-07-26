181d7cf36d26d1136ef3eff91135eb91f928c5e7
mokou <mokou@fastmail.com>
2022-07-26 16:23:43 +0200

    feat: update redirecting code

--- src/redirect.js
+++ src/redirect.js
@@ -1,11 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(301, {
-    Location: `https://eureka.fernehalwes.org/ovnitimer${
-      req.url === "/" ? "" : req.url
-    }`,
-  });
+  if (req.url.split("/")[0] === "f") {
+    const last = req.url.split("/").slice(1);
+    res.writeHead(301, {
+      Location: `https://eureka.fernehalwes.org/fairies${last.join("/")}`,
+    });
+  } else {
+    res.writeHead(301, {
+      Location: `https://eureka.fernehalwes.org/ovnitimer${
+        req.url === "/" ? "" : req.url
+      }`,
+    });
+  }
+
   res.end();
 });
 
